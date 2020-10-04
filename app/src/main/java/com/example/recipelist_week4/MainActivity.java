a4fb5aac4848d0c26b28be4eaab5a1e00286919b
Emre Ergul <eergul@ryerson.ca>
2020-10-04 18:37:10 -0400

    Initial commit

---
+++ app/src/main/java/com/example/recipelist_week4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.recipelist_week4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
